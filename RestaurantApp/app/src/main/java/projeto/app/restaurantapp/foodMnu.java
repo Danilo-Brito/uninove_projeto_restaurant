2a79ee766187701879e7a28c027767a9356a69b0
Danilo Brito <61256061+Danilo-Brito@users.noreply.github.com>
2020-05-21 16:25:18 -0300

    RecyclerView Food.

--- RestaurantApp/app/src/main/java/projeto/app/restaurantapp/foodMnu.java
+++ RestaurantApp/app/src/main/java/projeto/app/restaurantapp/foodMnu.java
@@ -3,13 +3,22 @@ package projeto.app.restaurantapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class foodMnu extends Fragment {
+
+    View v;
+    private RecyclerView myrecyclerview;
+    private List<Food> lstFood;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -40,11 +49,29 @@ public class foodMnu extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
+        lstFood = new ArrayList<>();
+        lstFood.add(new Food("Arroz com legumes", "Arroz com legumes com carne de panela.",
+                "20.00",R.drawable.arroz));
+        lstFood.add(new Food("Feijoada","Feijoada com arroz branco, couve e farofa.",
+                "30.00",R.drawable.feijoada));
+        lstFood.add(new Food("Muqueca","Muqueca de camarão cozido ao leite de coco com pimentão.",
+                "30.00",R.drawable.moqueca));
+        lstFood.add(new Food("Macarrão","Macarrão com molho bolonhesa.",
+                "35.00",R.drawable.pasta));
+        lstFood.add(new Food("Salada","Salada de legumes com tabule.",
+                "10.00",R.drawable.salada));
+        lstFood.add(new Food("Virado Paulista","Tutu de feijão com arroz branco, bisteca, couve refogada, ovo e bacon.",
+                "15.00",R.drawable.virado));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_food_mnu, container, false);
+        v = inflater.inflate(R.layout.fragment_food_mnu, container, false);
+        myrecyclerview = (RecyclerView) v.findViewById(R.id.food_recyclerview);
+        RecyclerViewAdapter recyclerAdapter = new RecyclerViewAdapter(getContext(),lstFood);
+        myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        myrecyclerview.setAdapter(recyclerAdapter);
+        return v;
     }
+
 }
